db6a69663522987f970222525ce06070fdd84dd7
Lex <alexandrioaril14@gmail.com>
2026-07-16 11:12:14 +0700

    feat(phase-1): complete authentication and route protection setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->redirectUsersTo(function (Request $request) {
+            return route('admin.dashboard');
+        });
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
